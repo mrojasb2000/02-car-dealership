113be8e2ae494c87c610b7a84e2929da89ae729e
Mauricio Rojas <mrojasb2000@gmail.com>
2025-01-22 13:35:50 -0300

    chore: adding basic endpoint methods

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -13,5 +23,39 @@ export class CarsController {
   @Get(':id')
   getCarById(@Param('id', ParseIntPipe) id: number) {
     return this.carsService.findById(id);
+  }
+
+  @Post()
+  createCar(@Body() payload) {
+    return {
+      ok: true,
+      payload: payload,
+    };
+  }
+
+  @Patch(':id')
+  patchCar(@Param('id', ParseIntPipe) id: number, @Body() payload) {
+    return {
+      ok: true,
+      id,
+      payload,
+    };
+  }
+
+  @Put(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() payload) {
+    return {
+      ok: true,
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      ok: true,
+      id,
+    };
   }
 }
